d3c34954ed538f0d14aac7a3dce96d18815a7468
Stig Lindqvist <stojg.lindqvist@gmail.com>
2015-03-27 12:41:18 +1300

    Auto single saml sign on

--- code/authenticators/SAMLSecurityExtension.php
+++ code/authenticators/SAMLSecurityExtension.php
@@ -6,7 +6,10 @@ class SAMLSecurityExtension extends Extension {
 	 * Will redirect the user directly to the ADFS if:
 	 * 1) the 'SAMLAuthenticator' is the default authenticator
 	 * 2) there isn't a GET param showloginform set to 1
-	 *
+	 * 3) the member is currently logged in
+	 * 4) the URL contains Security/login
+	 * 5) there are messages form messages
+	 * 
 	 * @return void
 	 */
 	public function onBeforeSecurityLogin() {
@@ -14,12 +17,39 @@ class SAMLSecurityExtension extends Extension {
 		if(Authenticator::get_default_authenticator() != 'SAMLAuthenticator') {
 			return;
 		}
-		// if there is a backdoor, don't redirect
+
+		// force the login of the loginform
 		if(isset($_GET['showloginform']) && $_GET['showloginform'] == 1) {
 			return;
 		}
 
+		// if member is already logged in, don't auto-sign-on
+		$member = Member::currentUser();
+		if($member && $member->exists()) {
+			return;
+		}
+
+		if(!empty($_REQUEST['url']) && stristr($_REQUEST['url'], 'Security/login')) {
+			return;
+		}
+		// if there are form messages, don't auto-sign-on
+		if(Session::get('FormInfo')) {
+			// since FormInfo can be a "nulled" array, we have to check
+			foreach(Session::get('FormInfo') as $form => $info) {
+				foreach($info as $name => $value) {
+					if($value !== null) {
+						return;
+					}
+				}
+			}
+		}
+
+		$backURL = Session::get('BackURL');
+		if(isset($_REQUEST['BackURL'])) {
+			$backURL = $_REQUEST['BackURL'];
+		}
+
 		$authenticator = Injector::inst()->create('SAMLAuthenticator');
-		$authenticator->authenticate(array());
+		$authenticator->authenticate(array("BackURL" => $backURL));
 	}
 }
